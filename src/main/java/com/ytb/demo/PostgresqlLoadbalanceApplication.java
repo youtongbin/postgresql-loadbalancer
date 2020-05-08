239d75b10137de102403d27f15bed9f3560fb17a
youtongbin <2232146813@qq.com>
2020-05-08 16:44:02 +0800

    init

---
+++ src/main/java/com/ytb/demo/PostgresqlLoadbalanceApplication.java
@@ -0,0 +1,13 @@
+package com.ytb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlLoadbalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlLoadbalanceApplication.class, args);
+	}
+
+}
